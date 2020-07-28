1c8da09d7d44d1857794c2c031999a5c25e75bec
AlexeiRadionov <radionov-aleksei@mail.ru>
2020-07-28 19:04:19 +0600

    fixed autoloaded classes

--- PHP/advancedLevel/galleryShop/public/index.php
+++ PHP/advancedLevel/galleryShop/public/index.php
@@ -3,9 +3,13 @@ session_start();
 //подключаем базовый класс моделей
 include '../model/Gallery.class.php';
 //подключаем автозагрузчик классов
-function __autoload($className) {
-	include "../model/$className.class.php";
-}
+spl_autoload_register(function($className) {
+	if (file_exists("../model/$className.class.php")) {
+		include "../model/$className.class.php";
+	} else if (file_exists("../Twig/$className.class.php")) {
+		include "../Twig/$className.class.php";
+	}
+});
 //подключаем контроллеры
 include '../controller/ControllerMain.class.php';
 include '../controller/ControllerAddFeedback.class.php';
