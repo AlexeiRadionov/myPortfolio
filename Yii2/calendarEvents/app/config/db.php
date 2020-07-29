fad89eb1854b05479ac7f709e27b9595c106031f
RadionovAlexey <radionov-aleksei@mail.ru>
2020-07-29 11:26:18 +0300

    fix

--- Yii2/calendarEvents/app/config/db.php
+++ Yii2/calendarEvents/app/config/db.php
@@ -7,7 +7,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=calendar',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
